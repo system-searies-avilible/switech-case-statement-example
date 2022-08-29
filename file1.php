9b86806f25c3ed42ea1b895453c701a76d128269
system-searies-avilible <111744571+system-searies-avilible@users.noreply.github.com>
2022-08-29 11:16:48 +0400

    Add files via upload

---
+++ file1.php
@@ -0,0 +1,32 @@
+<?php
+$weeks_days=5;
+
+switch($weeks_days){
+	case 1:
+echo"Today is Monday";
+break;
+	case 2:
+echo"Today is Tuesday";
+break;
+case 3:
+echo"Today is wensday";
+break;
+
+case 4:
+echo"Today is Thusday";
+break;
+case 5:
+echo"Today is Friday";
+break;
+
+case 6:
+echo"Today is Saterday";
+break;
+case 7:
+echo"Today is Sunday";
+break;
+default:
+echo"Please enter a valid day number";
+
+}
+?>
